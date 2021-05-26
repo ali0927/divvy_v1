e8024b1b37cb0e76c8dfa88d07b7362c41ccb2a3
ali0927 <kevinali0927@gmail.com>
2021-05-27 06:46:41 +0800

    escrow(house) pool

--- libraries/LibOutcome.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity 0.5.16;
-pragma experimental ABIEncoderV2;
-
-
-/// @title LibOutcome
-/// @notice Enumerations and utilities related to voting.
-library LibOutcome {
-    enum Outcome { VOID, OUTCOME_ONE, OUTCOME_TWO }
-}
