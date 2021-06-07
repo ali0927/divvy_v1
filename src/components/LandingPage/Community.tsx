96a249c195e9745062c008c108c66bcd5d3208b9
ali0927 <kevinali0927@gmail.com>
2021-06-07 08:22:29 +0200

    landing page corrections, replaced the default react favicon and changed the page title

--- src/components/LandingPage/Community.tsx
+++ src/components/LandingPage/Community.tsx
@@ -9,20 +9,14 @@ export const Community = () => {
           <div className="content">
             <h2 className="heading-md">Join the Community</h2>
             <p>
-              Divvy is a global, decentralized network with members from all around the world. Join the discussions on Social Networks and stay up to date with the latest news and announcements.
-            </p>
-            <p className="social-links">
-              <a href="https://discord.gg/QbWgYfYB" target="_blank" rel="noreferrer">discord</a>
-              <a href="https://twitter.com/DivvyBet" target="_blank" rel="noreferrer">twitter</a><br />
-              <a href="https://t.me" target="_blank" rel="noreferrer">telegram</a>
-              <a href="https://github.com/DivvyBet" target="_blank" rel="noreferrer">github</a>
+              Divvy is a global, decentralized network with members from all around the world. Join the discussions on our Social Networks and stay up to date with the latest news and announcements.
             </p>
           </div>
           <div className="social">
               <a className="justify-start align-start" href="https://discord.gg/QbWgYfYB" target="_blank" rel="noreferrer"><img src={discord} alt="Discord" /></a>
               <a className="justify-end align-start" href="https://twitter.com/DivvyBet" target="_blank" rel="noreferrer"><img src={twitter} alt="Twitter" /></a>
               <a className="justify-start align-end" href="https://github.com/DivvyBet" target="_blank" rel="noreferrer"><img src={github} alt="Github" /></a>
-              <a className="justify-end align-end" href="https//t.me/" target="_blank" rel="noreferrer"><img src={telegram} alt="Telegram" /></a>
+              <a className="justify-end align-end" href="https://t.me/Divvy_bet" target="_blank" rel="noreferrer"><img src={telegram} alt="Telegram" /></a>
           </div>
       </section>
     );
--- src/components/LandingPage/Header.tsx
+++ src/components/LandingPage/Header.tsx
@@ -1,16 +1,20 @@
 import logo from '../../img/landing-page/divvy_logo.svg'
+import discord from '../../img/landing-page/discord.png'
+import twitter from '../../img/landing-page/twitter.png'
+import github from '../../img/landing-page/github.png'
+import telegram from '../../img/landing-page/telegram.png'
 
 export const Header = () => {
     return (
       <header className="landing-page__header">
           <div className="logo"><a href="/"><img src={logo} alt="Divvy" /></a></div>
           <div className="tagline">Be the House<br />Decentralized betting</div>
-          <div className="message">Coming late 2021.<br />Follow the development:</div>
+          <div className="message">Coming late 2021</div>
           <div className="social-links">
-            <a href="https://discord.gg/QbWgYfYB" target="_blank" rel="noreferrer">discord</a>
-            <a href="https://twitter.com/DivvyBet" target="_blank" rel="noreferrer">twitter</a><br />
-            <a href="https://t.me" target="_blank" rel="noreferrer">telegram</a>
-            <a href="https://github.com/DivvyBet" target="_blank" rel="noreferrer">github</a>
+            <a href="https://discord.gg/QbWgYfYB" target="_blank" rel="noreferrer"><img src={discord} alt="Discord" /></a>
+            <a href="https://twitter.com/DivvyBet" target="_blank" rel="noreferrer"><img src={twitter} alt="Twitter" /></a>
+            <a href="https://t.me/Divvy_bet" target="_blank" rel="noreferrer"><img src={telegram} alt="Telegram" /></a>
+            <a href="https://github.com/DivvyBet" target="_blank" rel="noreferrer"><img src={github} alt="Github" /></a>
           </div>
       </header>
     );
