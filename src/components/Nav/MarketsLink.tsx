71d6531d28b0255ea99d82c80a2985210174431a
ali0927 <kevinali0927@gmail.com>
2021-06-07 20:43:48 +0530

    added search

--- src/components/Nav/MarketsLink.tsx
+++ src/components/Nav/MarketsLink.tsx
@@ -2,30 +2,50 @@ import React, { useState } from 'react';
 import { Input, Button } from 'antd'
 import { SearchOutlined } from "@ant-design/icons"
 export const MarketsLink = () => {
-    const markets = [{ value: "football", name: "Football" }, { value: "basketball", name: "Basketball" }, { value: "mma", name: "MMA" },];
+    const marketseg = [{ value: "football", name: "Football", }, { value: "basketball", name: "Basketball" }, { value: "mma", name: "MMA" },
+    { value: "americanfootball", name: "American Football" },
+    { value: "basketball", name: "Basketball" },
+    { value: "hockey", name: "Hockey" },
+    { value: "esports", name: "E-Sports" },
+    { value: "hoor", name: "Horse Racing" },
+    { value: "fighting", name: "Fighting" },
+    { value: "motorracing", name: "Motor Racing" },
+    { value: "tennis", name: "Tennis" },
+    { value: "golf", name: "Golf" },
+    { value: "darts", name: "Darts" },
+    { value: "rugby", name: "Rugby" },
+    { value: "tabletennis", name: "Table Tennis" },
+    { value: "cricket", name: "Cricket" },
+    { value: "snooker", name: "Snooker" },
+    { value: "volleyball", name: "Volleyball" },
+    { value: "cycling", name: "Cycling" },];
+    const [markets, setMarkets] = useState(marketseg)
+    const [search, setSearch] = useState("")
     const MarketsUI = () => {
         let market = new Array();
         markets.map((data, index) => {
-            market.push(
-                <div className="search-item">
-                    <Button className="search-button" ghost type="default">
-                        <div className="search-button-data">
-                            <div className="search-left">
-                                {data.name}
+            if (data.name.toLowerCase().includes(search.toLowerCase())) {
+                market.push(
+                    <div className="search-item">
+                        <Button className="search-button" ghost type="default">
+                            <div className="search-button-data">
+                                <div className="search-left">
+                                    {data.name}
+                                </div>
+                                <div className="search-right">
+                                    {parseInt(String((Math.random() + 1) * 10))}
+                                </div>
                             </div>
-                            <div className="search-right">
-                                {23}
-                            </div>
-                        </div>
-                    </Button>
-                </div>
-            )
+                        </Button>
+                    </div>
+                )
+            }
         })
         return market;
     }
     return (
         <div>
-            <Input style={{ border: "0px", marginLeft: "1px", outline: "1px solid #1f1f1f", height: "40px", width: "20em" }} placeholder={"Search for bets"} prefix={<SearchOutlined />} />
+            <Input value={search} onChange={(event) => setSearch(event.currentTarget.value)} style={{ border: "0px", marginLeft: "1px", outline: "1px solid #1f1f1f", height: "40px", width: "20em" }} placeholder={"Search for bets"} prefix={<SearchOutlined />} />
             <div className="sidebar-section">
                 {MarketsUI()}
             </div>
