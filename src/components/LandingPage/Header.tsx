95385a710adaf299ff86709ff1d61fdefaec3de9
ali0927 <kevinali0927@gmail.com>
2021-06-07 19:45:33 +0200

    Updated roadmap and "be the house" section. Updated header and footer.

--- src/components/LandingPage/Header.tsx
+++ src/components/LandingPage/Header.tsx
@@ -6,8 +6,8 @@ export const Header = () => {
     return (
       <header className="landing-page__header">
           <div className="logo"><a href="/"><img src={logo} alt="Divvy" /></a></div>
-          <div className="tagline">Be the House<br />Decentralized betting</div>
-          <div className="message">Coming late 2021</div>
+          {/*<div className="tagline">Be the House<br />Decentralized betting</div>
+          <div className="message">Coming late 2021</div>*/}
           <div className="social-links">
             <a href="https://discord.gg/QbWgYfYB" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faDiscord} /></a>
             <a href="https://twitter.com/DivvyBet" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faTwitter} /></a>
