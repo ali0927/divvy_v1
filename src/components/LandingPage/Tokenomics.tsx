9484776674e2eacd785d5297d1570fff59438098
ali0927 <kevinali0927@gmail.com>
2021-06-07 13:14:08 +0200

    Tokenomics - added back the legend

--- src/components/LandingPage/Tokenomics.tsx
+++ src/components/LandingPage/Tokenomics.tsx
@@ -5,16 +5,16 @@ export const Tokenomics = () => {
         <section className="landing-page__tokenomics">
           <div className="content">
             <h2 className="heading-md">Divvy Token Economics</h2>
-            {/*<ul>
-              <li className="team">15.0% Team &amp; Advisors</li>
-              <li className="foundation">5.0% Foundation</li>
-              <li className="private-public">16.5% Private &amp; Public</li>
-              <li className="community">38.0% Community &amp; Ecosystem</li>
-              <li className="marketing">10.0% Partnership &amp; Marketing</li>
-              <li className="dev-grants">10.0% Dev Grants</li>
-              <li className="liquidity">1.8% Liquidity</li>
-              <li className="reserves">3.7% Reserves</li>
-            </ul>*/}
+            <ul>
+              <li className="team">Team &amp; Advisors</li>
+              <li className="foundation">Foundation</li>
+              <li className="private-public">Private &amp; Public</li>
+              <li className="community">Community &amp; Ecosystem</li>
+              <li className="marketing">Partnership &amp; Marketing</li>
+              <li className="dev-grants">Dev Grants</li>
+              <li className="liquidity">Liquidity</li>
+              <li className="reserves">Reserves</li>
+            </ul>
           </div>
           <div className="chart">
             <Chart
