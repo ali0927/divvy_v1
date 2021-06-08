d0c5ca64ce3f0f785207f647b791b1205ba5f5d5
ali0927 <kevinali0927@gmail.com>
2021-06-08 11:41:21 +0530

    added pdf

--- src/components/LandingPage/Download.tsx
+++ src/components/LandingPage/Download.tsx
@@ -4,9 +4,9 @@ class Download extends Component {
   render() {
 
     return (
-        <div className = "App">
-          <a href = {"/litepaper.pdf"} target = "_blank">Litepaper</a>
-        </div>
+      <div className="App">
+        <a href={process.env.PUBLIC_URL + "/litepaper.pdf"} target="_blank">Litepaper</a>
+      </div>
     );
   }
 }
