448772f334ee8b473e23eb432f0e5e0eb2531ae3
ali0927 <kevinali0927@gmail.com>
2021-06-12 12:16:59 +0530

    removed magic string

--- src/contexts/chainselect.tsx
+++ src/contexts/chainselect.tsx
@@ -1,5 +1,6 @@
 import React, { useState, createContext, useEffect } from "react"
 import { ChainType } from "../constants/chains";
+import { LABELS } from "../constants/labels";
 export const ChainSelectContext = createContext<any>(null);
 
 export const ChainProvider = (props: { children: any }) => {
@@ -7,7 +8,7 @@ export const ChainProvider = (props: { children: any }) => {
         useState<ChainType>();
     useEffect(() => {
         const getChain = async () => {
-            const chain: any = await localStorage.getItem("chain")
+            const chain: any = await localStorage.getItem(LABELS.CHAIN_STRING)
             if (chain) {
                 setChain(chain)
             } else {
@@ -17,7 +18,7 @@ export const ChainProvider = (props: { children: any }) => {
         getChain()
     }, [])
     function changeChain(chain: ChainType) {
-        localStorage.setItem("chain", chain)
+        localStorage.setItem(LABELS.CHAIN_STRING, chain)
         setChain(chain);
     };
     return (
