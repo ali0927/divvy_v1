8df4a515a3dcb8d44c1049a2593d39153be598bf
ali0927 <kevinali0927@gmail.com>
2021-06-12 02:11:49 +0200

    Removed "Divy is" from the "Difference" cards (#49)

--- src/components/LandingPage/DifferenceCard.tsx
+++ src/components/LandingPage/DifferenceCard.tsx
@@ -11,7 +11,7 @@ export const DifferenceCard = (
                 <img src={props.image} alt={"Divvy is "+props.title} className="img-fluid" />
             </div>
             <div className="card-body">
-                <h3 className="heading-sm"><span className="text-muted">Divvy is</span><br />{props.title}</h3>
+                <h3 className="heading-sm">{props.title}</h3>
                 <p>{props.description}</p>
             </div>
         </div>
