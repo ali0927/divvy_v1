f27eeba7fe5887399660d602884ab80b98f055e6
ali0927 <kevinali0927@gmail.com>
2021-07-12 18:15:16 -0700

    Odds button active only when the bet is in betslip (#69)
    * Odds button active only when the bet is in betslip
    
    * Clicking a sport will take you to the bets page.
    Use <Link> over <a> to avoid a page refresh in app.

--- src/components/SingleMarket/OddsSelection.tsx
+++ src/components/SingleMarket/OddsSelection.tsx
@@ -1,48 +1,61 @@
-import { useState, useContext } from "react";
-import { Col, Row } from "antd";
-import { Odds, BetType, Market, Team, Bet, MarketSide, BetStatus } from "../../constants";
+import { useContext } from "react";
+import { Button, Col, Row } from "antd";
+import { Odds, BetType, Market, Bet, MarketSide, BetStatus } from "../../constants";
 import { BetsContext } from "../../contexts/bets";
 export const OddsSelection = (props: { market: Market, selection: string, odds: Odds, otherTeam: string, selectionTeam: string, marketSide: MarketSide }) => {
-    const [selection, setSelection] = useState("")
     const bets = useContext(BetsContext);
 
-
-    const setSlip = (betType: BetType, odds: number, oddsFeed: string, pubkey: string) => {
-        setSelection(betType)
-        let bet: Bet;
-        bet = {
-            betId: Math.random() * 10,
-            marketId: props.market.marketId,
-            seasonId: props.market.seasonId,
-            sportId: props.market.sportId,
-            marketPubkey: pubkey,
-            risk: 0,
-            payout: 0,
-            userPubkey: "uerguerbgub",
-            selection: props.selection,
-            odds: odds,
-            status: BetStatus.Current,
-            market: props.market,
-            oddsPubKey: oddsFeed,
-            selectionTeam: props.selectionTeam,
-            otherTeam: props.otherTeam,
-            marketSide: props.marketSide,
-            betType: betType,
-            sportName: props.market.sportName,
-            seasonName: props.market.seasonName,
+    const setSlip = (betType: BetType, odds: number, oddsFeed: string, marketPubkey: string) => {
+        if (containsBet(betType)) {
+            const existing = bets?.findBets(props.market.marketId, props.marketSide, betType, BetStatus.Current);
+            if(existing && existing.length !== 0) {
+                bets?.removeBet(existing[0].betId);
+            }
         }
-        bets?.addBet(bet)
+        else {
+            let bet: Bet;
+            bet = {
+                betId: Math.random() * 10,
+                marketId: props.market.marketId,
+                seasonId: props.market.seasonId,
+                sportId: props.market.sportId,
+                marketPubkey: marketPubkey,
+                risk: 0,
+                payout: 0,
+                userPubkey: "uerguerbgub",
+                selection: props.selection,
+                odds: odds,
+                status: BetStatus.Current,
+                market: props.market,
+                oddsPubKey: oddsFeed,
+                selectionTeam: props.selectionTeam,
+                otherTeam: props.otherTeam,
+                marketSide: props.marketSide,
+                betType: betType,
+                sportName: props.market.sportName,
+                seasonName: props.market.seasonName,
+            }
+            bets?.addBet(bet)
+        }
     }
+
+    const containsBet = (betType: BetType) => {
+        return bets?.containsBet(props.market.marketId, props.marketSide, betType, BetStatus.Current) ?? false;
+    }
+
     return (
         <Row style={{ display: "flex", alignItems: "center", height: 36, marginLeft: 20, marginTop: 10 }}>
-            <Col span={8} onClick={() => setSlip(BetType.moneyline, props.odds.moneyline, props.odds.moneylineFeedPubkey, props.market.moneylineMarketPubkey)} className={selection === 'moneyline' ? "odds odds-active" : "odds"}>
-                <b>{`${props.odds.moneyline >= 0 ? "+" : ""}${props.odds.moneyline}`}</b>
+            <Col span={8} onClick={() => setSlip(BetType.moneyline, props.odds.moneyline, props.odds.moneylineFeedPubkey, props.market.moneylineMarketPubkey)}
+                    className={containsBet(BetType.moneyline) ? "odds odds-active" : "odds"}>
+                    <b>{`${props.odds.moneyline >= 0 ? "+" : ""}${props.odds.moneyline}`}</b>
             </Col>
-            <Col span={8} onClick={() => setSlip(BetType.spread, props.odds.spread, props.odds.spreadFeedPubkey, props.market.spreadMarketPubKey)} className={selection === 'spread' ? "odds odds-active" : "odds"}>
-                <b>{`(${props.odds.spreadPoints >= 0 ? "+" : ""}${String(props.odds.spreadPoints)}) ${props.odds.spread >= 0 ? "+" : ""}${String(props.odds.spread)}`}</b>
+            <Col span={8} onClick={() => setSlip(BetType.spread, props.odds.spread, props.odds.spreadFeedPubkey, props.market.spreadMarketPubKey)}
+                    className={containsBet(BetType.spread) ? "odds odds-active" : "odds"}>
+                    <b>{`(${props.odds.spreadPoints >= 0 ? "+" : ""}${String(props.odds.spreadPoints)}) ${props.odds.spread >= 0 ? "+" : ""}${String(props.odds.spread)}`}</b>
             </Col>
-            <Col span={8} onClick={() => setSlip(BetType.total, props.odds.total, props.odds.totalFeedPubkey, props.market.totalMarketPubkey)} className={selection === 'total' ? "odds odds-active" : "odds"}>
-                <b>{`(${props.odds.totalPoints >= 0 ? "O" : "U"} ${Math.abs(props.odds.totalPoints)}) ${props.odds.total >= 0 ? "+" : ""}${props.odds.total}`}</b>
+            <Col span={8} onClick={() => setSlip(BetType.total, props.odds.total, props.odds.totalFeedPubkey, props.market.totalMarketPubkey)}
+                    className={containsBet(BetType.total) ? "odds odds-active" : "odds"}>
+                    <b>{`(${props.odds.totalPoints >= 0 ? "O" : "U"} ${Math.abs(props.odds.totalPoints)}) ${props.odds.total >= 0 ? "+" : ""}${props.odds.total}`}</b>
             </Col>
         </Row>
     )
