e4eb5d6b6827b4e31a3e873c1f861329b120975f
ali0927 <kevinali0927@gmail.com>
2021-07-16 11:41:36 -0700

    Now divvy state id. Remove settle bets buttons (#70)

--- src/components/Bets/PendingBets.tsx
+++ src/components/Bets/PendingBets.tsx
@@ -1,21 +1,13 @@
-import { Button, Collapse } from "antd";
+import { Collapse } from "antd";
 import { GameTeams } from "./GameTeams";
 import { PotentialWins } from "./PotentialWins";
 import { ConfirmedOdds } from "./ConfirmedOdds";
-import { Bet, BetStatus, BetType } from "../../constants";
-import LinkLabel from "../Nav/LinkLabel";
+import { Bet, BetStatus } from "../../constants";
 import { useContext } from "react";
 import { BetsContext } from "../../contexts/bets";
 export const PendingBets = () => {
     const bets = useContext(BetsContext);
 
-    var pendingBets = 0
-    bets?.bets.forEach((value: Bet) => {
-      if (value.status === BetStatus.Pending)
-      {
-        pendingBets++;
-      }
-    })
     return (
         <div style={{display: "flex", flexDirection: "column"}}>
             <Collapse accordion={true} style={{ display: "grid" }} ghost={true} expandIconPosition="right">
@@ -30,19 +22,6 @@ export const PendingBets = () => {
                     }
                 })}
             </Collapse>
-            {pendingBets !== 0 ? <div style={{display: "flex", alignContent: "flex-end"}}>
-                <Button onClick={() => bets?.settleBets("win")} className="ant-btn-active" style={{ justifyContent: "flex-end", width:"50%"}}>
-                    <LinkLabel>
-                        Win Bets
-                    </LinkLabel>
-                </Button>
-                <Button onClick={() => bets?.settleBets("lose")} className="ant-btn-active" style={{ justifyContent: "flex-end", width:"50%"}}>
-                    <LinkLabel>
-                        Lose Bets
-                    </LinkLabel>
-                </Button>
-            </div>
-            : <></>}
         </div>
     );
 };
