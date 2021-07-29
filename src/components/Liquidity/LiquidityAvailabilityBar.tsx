865a070c3e2621d5736ad79d66b18102241fea39
ali0927 <kevinali0927@gmail.com>
2021-07-29 20:22:55 +0530

    Fixed Progress bar misalignment

--- src/components/Liquidity/LiquidityAvailabilityBar.tsx
+++ src/components/Liquidity/LiquidityAvailabilityBar.tsx
@@ -1,7 +1,27 @@
+import { Component } from 'react';
 import { Progress } from "antd";
+import { CircularProgressbarWithChildren } from 'react-circular-progressbar';
 import { useState, useEffect, useContext } from "react";
 import { HousePoolContext } from "../../contexts/sol/hpliquidity";
 import { HousePoolStateContext } from "../../contexts/sol/hpstate";
+
+class GradientSVG extends Component<{ startColor: string, endColor: string, rotation: number }, {}> {
+  render() {
+    let gradientTransform = `rotate(${this.props.rotation})`;
+
+    return (
+      <svg style={{ height: 0 }}>
+        <defs>
+          <linearGradient id={"gradient-progress"} gradientTransform={gradientTransform}>
+            <stop offset="0%" stopColor={this.props.startColor} />
+            <stop offset="100%" stopColor={this.props.endColor} />
+          </linearGradient>
+        </defs>
+      </svg>
+    );
+  }
+}
+
 
 export const LiquidityAvailabilityBar = () => {
   const [width, setWindowWidth] = useState(0);
@@ -19,20 +39,19 @@ export const LiquidityAvailabilityBar = () => {
     };
   }, [])
   return (
-    <Progress
-      type="dashboard"
-      strokeColor={{
-        "0%": "#04f35e",
-        "100%": "#4b54cd",
-      }}
-      strokeWidth={15}
-      percent={((htBalance - bettorBalance - liveLiquidity - lockedLiquidity) * 100) / (htBalance - bettorBalance)}
-      strokeLinecap={"square"}
-      width={width}
-      format={() => {
-        return <video autoPlay={true} loop={true} width={width/3} height={width/3} src={"https://siasky.net/AABLoM1oiF4w42__V4_9CO1M1AhNGydb5oaYL0vgiRpFjQ"} />;
-      }}
-      showInfo={true}
-    />
+    <div style={{transform: "rotate(225deg)"}}>
+      <GradientSVG startColor={"#7c01ff"} endColor={"#00d77d"} rotation={90} />
+      <CircularProgressbarWithChildren
+        value={((htBalance - bettorBalance - liveLiquidity - lockedLiquidity) * 100) / (htBalance - bettorBalance)}
+        circleRatio={0.75}
+        strokeWidth={15}
+        styles={{
+          path: { stroke: `url(#gradient-progress)`, height: '100%', transition: 'stroke-dashoffset 0.5s ease 0s', }, 
+          trail: { stroke: "#242424" }
+        }}
+      >
+        <video autoPlay={true} style={{transform: "rotate(-225deg)"}} loop={true} width={width/3} height={width/3} src={"https://siasky.net/AABLoM1oiF4w42__V4_9CO1M1AhNGydb5oaYL0vgiRpFjQ"} />
+      </CircularProgressbarWithChildren>
+    </div>
   );
 };
