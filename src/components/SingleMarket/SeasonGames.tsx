c71425753144a93f8d92d3076a33686b8ec49ac4
ali0927 <kevinali0927@gmail.com>
2021-08-17 11:30:02 +0900

    insert sports list and search box

--- src/components/SingleMarket/SeasonGames.tsx
+++ src/components/SingleMarket/SeasonGames.tsx
@@ -4,19 +4,24 @@ import { SeasonName } from "./SeasonName";
 import { Market, Season } from "../../constants";
 import { Fragment } from "react";
 const { Panel } = Collapse;
-export const SeasonGames = (props: { season: Season }) => {
+export const SeasonGames = (props: { season: Season, search: string }) => {
   return (
     <div className="single-market">
       <Row>
         <Col span={24}>
           <Collapse defaultActiveKey={"1"} accordion={true} style={{ display: "grid" }} ghost={true} expandIconPosition="right">
             <Panel header={<SeasonName name={props.season.season.seasonName} matches={props.season.markets.length} />} key="1">
-              {props.season.markets.map((value: Market, index: number) => (
-                <Fragment key={value.marketId}>
-                  <SingleMatchComponent market={value} />
-                  <Divider style={{ color: "gray" }} />
-                </Fragment>
-              ))}
+              {props.season.markets.map((value: Market, index: number) => {
+                if(value.teamA.concat(value.teamB).toLowerCase().includes(props.search.toLowerCase())) {
+                  return (
+                    <Fragment key={value.marketId}>
+                      <SingleMatchComponent market={value} />
+                      <Divider style={{ color: "gray" }} />
+                    </Fragment>
+                  )
+                }
+              }      
+              )}
             </Panel>
           </Collapse>
         </Col>
--- src/components/SingleMarket/SeasonHeader.tsx
+++ src/components/SingleMarket/SeasonHeader.tsx
@@ -1,5 +1,14 @@
-export const SeasonHeader = (props: { seasonName: string | undefined }) => {
+import { Input } from 'antd'
+import { SearchOutlined } from "@ant-design/icons"
+
+export const SeasonHeader = (props: { seasonName: string | undefined, onChange: any }) => {
     return (
+      <div style={{display:'flex', justifyContent:'space-between', alignItems:'center'}}>
         <h1>{props.seasonName}</h1>
+        <Input  
+            style={{border:"0px", padding:"1em", marginTop:"1px", outline:"1px solid #1f1f1f", height:"40px", width:"15vw"}}
+            placeholder={"Search for bets"} prefix={<SearchOutlined />}
+            onChange={(event) => props.onChange(event.currentTarget.value)} />
+      </div>
     )
 };
