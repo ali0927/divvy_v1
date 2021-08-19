82e5eeb528fcc3d8635b3beab05ff56b4406cc4b
ali0927 <kevinali0927@gmail.com>
2021-08-19 16:41:16 +0900

    ability to add pyout and change risk accordingly

--- src/components/Bets/MyBet.tsx
+++ src/components/Bets/MyBet.tsx
@@ -8,6 +8,7 @@ import { BetsContext } from "../../contexts/bets";
 import { americanToDecimal, LAMPORTS_PER_USDT, tokenAmountToString } from "../../constants";
 export const MyBet = (props: { bet: Bet }) => {
     const [riskStr, setRiskStr] = useState("0")
+    const [payoutStr, setPayoutStr] = useState("0")
     const [risk, setRisk] = useState(0)
     const bets = useContext(BetsContext);
 
@@ -22,43 +23,56 @@ export const MyBet = (props: { bet: Bet }) => {
             parsedRisk = 0;
         }
         setRiskStr(risk);
+        setPayoutStr(tokenAmountToString(parsedRisk * (americanToDecimal(props.bet.odds))))
+        setRisk(parsedRisk);
+        bets?.editBetRisk(props.bet.betId, parsedRisk)
+    }
+    const doSetRiskByPayout = (payout: string) => {
+      let risk = parseFloat(payout) / americanToDecimal(props.bet.odds)
+      let parsedRisk = risk * LAMPORTS_PER_USDT;
+        if (isNaN(parsedRisk)) {
+            parsedRisk = 0;
+            setRiskStr("0");
+        }
+        else setRiskStr(risk.toString());
+        setPayoutStr(payout)
         setRisk(parsedRisk);
         bets?.editBetRisk(props.bet.betId, parsedRisk)
     }
 
     return (
-        <div style={{ margin: 20 }}>
-            <div style={{ display: "flex", justifyContent: "space-between" }}>
-                <GameTeams selectionTeam={props.bet.selectionTeam} otherTeam={props.bet.otherTeam} betSlip={props.bet} />
-                <CloseOutlined onClick={() => bets?.removeBet(props.bet.betId)} style={{ marginTop: 5 }} />
-            </div>
-            <ConfirmedOdds betType={props.bet.betType} bet={props.bet} />
-            <div style={{ display: "inline-flex" }}>
-                <Form.Item
-                    style={{ width: "50%", marginRight: 4 }}
-                    label="Wager"
-                    rules={[
-                        {
-                            required: true,
-                            message: "Please enter an amount",
-                        },
-                    ]}
-                >
-                    <Input value={riskStr} onChange={(event) => { doSetRisk(event.currentTarget.value) }} />
-                </Form.Item>
-                <Form.Item
-                    style={{ width: "50%", marginLeft: 4 }}
-                    label="Payout"
-                    rules={[
-                        {
-                            required: true,
-                            message: "Please enter your house pool address.",
-                        },
-                    ]}>
-                    <Input color={"white"} style={{ color: "white" }} placeholder={tokenAmountToString(risk * (americanToDecimal(props.bet.odds)))} disabled={true} />
-                </Form.Item>
-            </div>
-            <Divider style={{ marginTop: 0 }} />
+      <div style={{ margin: 20 }}>
+        <div style={{ display: "flex", justifyContent: "space-between" }}>
+            <GameTeams selectionTeam={props.bet.selectionTeam} otherTeam={props.bet.otherTeam} betSlip={props.bet} />
+            <CloseOutlined onClick={() => bets?.removeBet(props.bet.betId)} style={{ marginTop: 5 }} />
         </div>
+        <ConfirmedOdds betType={props.bet.betType} bet={props.bet} />
+        <div style={{ display: "inline-flex" }}>
+            <Form.Item
+                style={{ width: "50%", marginRight: 4 }}
+                label="Wager"
+                rules={[
+                    {
+                      required: true,
+                      message: "Please enter an amount",
+                    },
+                ]}
+            >
+                <Input value={riskStr} onChange={(event) => { doSetRisk(event.currentTarget.value) }} />
+            </Form.Item>
+            <Form.Item
+                style={{ width: "50%", marginLeft: 4 }}
+                label="Payout"
+                rules={[
+                    {
+                        required: true,
+                        message: "Please enter your house pool address.",
+                    },
+                ]}>
+                <Input color={"white"} style={{color:"white"}} value={payoutStr} onChange={(e)=>{doSetRiskByPayout(e.currentTarget.value)}}/>
+            </Form.Item>
+        </div>
+        <Divider style={{ marginTop: 0 }} />
+      </div>
     );
 };
