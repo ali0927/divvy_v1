0643e2470cb9679717bf67ea9288550b49e62b0c
ali0927 <kevinali0927@gmail.com>
2021-08-25 01:29:05 +0900

    change setting menu

--- src/components/Settings/SolSettings.tsx
+++ src/components/Settings/SolSettings.tsx
@@ -2,13 +2,37 @@ import React from "react";
 import { Select } from "antd";
 import { useConnectionConfig } from "../../contexts/sol/connection";
 import { ENDPOINTS } from "../../constants/sol/env";
-
+import { LANGUAGES, CURRENCIES } from "../../constants/settings";
 export const SolSettings = () => {
   const { endpoint, setEndpoint } = useConnectionConfig();
 
   return (
     <>
-      <div style={{ display: "grid" }}>
+      <div style={{display:"grid"}} className="sol-setting-item">
+        Display Currency:{" "}
+        <Select
+          value={CURRENCIES[0].name}
+        >
+          {CURRENCIES.map(({ name, label }) => (
+            <Select.Option value={name} key={name}>
+              {label}
+            </Select.Option>
+          ))}
+        </Select>       
+      </div>
+      <div style={{display:"grid", marginTop:'1rem'}} className="sol-setting-item">
+        Display Language:{" "}
+        <Select
+          value={LANGUAGES[0].name}
+        >
+          {LANGUAGES.map(({ name, label }) => (
+            <Select.Option value={name} key={name}>
+              {label}
+            </Select.Option>
+          ))}
+        </Select>       
+      </div>
+      <div style={{display:"grid", marginTop:'1rem'}} className="sol-setting-item">
         Network:{" "}
         <Select
           onSelect={setEndpoint}
