d69f9315a8c1d6f7483d8d89d075b0f8c7d5b0a8
ali0927 <kevinali0927@gmail.com>
2021-09-03 21:47:02 +0530

    fixed total; in divvy

--- src/components/Liquidity/LiquidityPool.tsx
+++ src/components/Liquidity/LiquidityPool.tsx
@@ -1,13 +1,18 @@
 import { useContext } from "react";
 import { tokenAmountToString } from "../../constants";
+import { BetStateContext } from "../../contexts/sol/betstate";
+import { BetPoolContext } from "../../contexts/sol/betusdt";
 import { HousePoolContext } from "../../contexts/sol/hpliquidity";
 
 export const LiquidityPool = () => {
   const { htBalance } = useContext(HousePoolContext);
+  const { liveLiquidity, lockedLiquidity } = useContext(BetStateContext)
+  const { bettorBalance } = useContext(BetPoolContext);
+
     return (
       <div className="liquidity-pool">
         <p className="text-primary">Total in Divvy</p>
-        <h2><span style={{ fontSize: "2rem", fontWeight: 800 }}>{tokenAmountToString(htBalance)}</span> USDC</h2>
+        <h2><span style={{ fontSize: "2rem", fontWeight: 800 }}>{tokenAmountToString(htBalance + lockedLiquidity + liveLiquidity + bettorBalance)}</span> USDC</h2>
       </div>
     );
 };
