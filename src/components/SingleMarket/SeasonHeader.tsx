7ab4c3570311d07f71c1f5adb3acfb281363f38b
ali0927 <kevinali0927@gmail.com>
2021-09-11 15:39:20 +0300

    chores: SeasonHeader inline styles refactoring and padding fix

--- src/components/SingleMarket/SeasonHeader.tsx
+++ src/components/SingleMarket/SeasonHeader.tsx
@@ -4,15 +4,32 @@ import { ReloadButton } from './ReloadButton'
 
 export const SeasonHeader = (props: { seasonName: string | undefined, onChange: any, refetch: any }) => {
     return (
-      <div style={{display:'flex', justifyContent:'space-between', alignItems:'center'}}>
+      <div style={style.wrapper}>
         <h1>{props.seasonName}</h1>
         <div className="balance-container">
-        <ReloadButton refetch={props.refetch} />
-        <Input  
-            style={{border:"0px", padding:"1em", marginTop:"1px", outline:"1px solid #1f1f1f", height:"40px", width:"15vw"}}
-            placeholder={"Search for bets"} prefix={<SearchOutlined />}
-            onChange={(event) => props.onChange(event.currentTarget.value)} />
+          <ReloadButton refetch={props.refetch} />
+          <Input  
+              style={style.input}
+              placeholder={"Search for bets"} prefix={<SearchOutlined />}
+              onChange={(event) => props.onChange(event.currentTarget.value)} />
         </div>
       </div>
     )
 };
+
+const style = {
+  wrapper: {
+    display:'flex', 
+    justifyContent:'space-between', 
+    alignItems:'center',
+    padding: '0 16px 0 13px'
+  },
+  input: {
+    border:"0px", 
+    padding:"1em", 
+    marginTop:"1px", 
+    outline:"1px solid #1f1f1f", 
+    height:"40px", 
+    width:"15vw"
+  }
+}
