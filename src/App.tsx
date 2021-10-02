6afafcfaff6d7f6d1faf6a948fa0a98b475ce4b8
ali0927 <kevinali0927@gmail.com>
2021-10-02 17:48:34 +0530

    Change GA tracking code

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ const LiquidityView = load(lazy(() => import("./views/LiquidityView")));
 const RootContextProvider = load(lazy(() => import("./contexts")));
 const BetsProvider = load(lazy(() => import("./contexts/bets")));
 function App() {
-  ReactGA.initialize("G-PNNLDPF5NP");
+  ReactGA.initialize("UA-208887655-1");
   const location = useLocation();
   useEffect(() => {
     ReactGA.pageview(window.location.pathname);
