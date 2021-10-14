82fc778a94dc7da3593b161055d8221aac37384c
ali0927 <kevinali0927@gmail.com>
2021-10-14 11:19:53 +0900

    remove flag and update bet/payout

--- src/views/Games/MoonShot/ChatRoom.tsx
+++ src/views/Games/MoonShot/ChatRoom.tsx
@@ -15,10 +15,6 @@ export const ChatRoom = () => {
     <div style={{position:'relative', height:'100%'}}>
       <div style={{display:'flex', justifyContent:'space-between', alignItems:'center', padding:'10px'}}>
         <strong style={{fontSize:'1.5em'}}>Chat</strong>
-        <div>
-          <img src="../us.svg" style={{width:'30px', height:'20px', margin:'auto 10px', borderRadius:'6px'}} alt="English" />
-          <img src="../ru.svg" style={{width:'30px', height:'20px', borderRadius:'6px'}} alt="Russian" />
-        </div>
       </div>
       <div style={{position:'absolute', top:'4em', bottom:'4em', padding:'10px', overflow:'scroll', width:'100%', borderTop:'1px solid var(--gray)', borderBottom:'1px solid var(--gray)'}}>
         {
@@ -33,9 +29,7 @@ export const ChatRoom = () => {
       </div>
       <div style={{position:'absolute', bottom:'0', display:'flex', alignItems:'center', padding:'10px', width:'100%', height:'4em'}}>
         <Input placeholder="Your message:" className="game-moonshot__input" value={message} onChange={e => { setMessage(e.currentTarget.value) }} />
-        <SendOutlined style={{margin:'10px', color:'var(--game-blue)', fontSize:'1.5em'}}/>
-        <Divider type="vertical" style={{height:'1.5em', margin:'0'}}/>
-        <strong style={{margin:'10px', fontSize:'1.2em', color:'var(--game-blue)'}}>LN</strong>
+        <SendOutlined style={{margin:'10px', cursor:'pointer', color:'var(--game-blue)', fontSize:'1.5em'}}/>
       </div>
     
     </div>
