4a38a9c142b6457a56b955d0057c932452495c58
ali0927 <kevinali0927@gmail.com>
2021-10-16 00:44:20 +0530

    Added custom usernames moonshot chatroom

--- src/components/Games/MoonShot/ChatRoom.tsx
+++ src/components/Games/MoonShot/ChatRoom.tsx
@@ -4,15 +4,22 @@ import { Input } from "antd";
 import {io} from "socket.io-client";
 import {ChatsModel} from "../../../models/games/moonshot/common";
 import {MoonshotSocketContext} from "../../../contexts/moonshot-socket";
+import {CHARACTER_FIRST_NAMES, CHARACTER_LAST_NAMES} from "../../../constants/games";
 
+const generateUsername = () => {
+    return CHARACTER_FIRST_NAMES[Math.floor(Math.random() * CHARACTER_FIRST_NAMES.length)]+" "+CHARACTER_LAST_NAMES[Math.floor(Math.random() * CHARACTER_LAST_NAMES.length)];
+}
+
+const username = generateUsername().toLowerCase().replace(" ", "-");
 export const ChatRoom = () => {
   const [message, setMessage] = useState('');
   const [chats, setChats] = useState<ChatsModel[]>([]);
+
   const socket = useContext(MoonshotSocketContext);
   const handleSend = () => {
-      if(message != "") {
+      if(message !== "") {
           socket.emit("new message", {
-              name: "Shashwat",
+              name: username,
               text: message,
               date: Date.now()
           })
@@ -25,7 +32,7 @@ export const ChatRoom = () => {
           if(temp.length > 200) {
               temp.pop();
           }
-          temp = [data, ...temp];
+          temp.push(data);
           setChats([ ...temp ]);
       })
       socket.on("all-msgs", data => {
@@ -37,15 +44,16 @@ export const ChatRoom = () => {
       socket.emit("get-msgs");
   }, [])
   return (
-    <div style={{position:'relative', height:'100%'}}>
+    <div style={{position:'relative', height:'98%'}}>
       <div style={{display:'flex', justifyContent:'space-between', alignItems:'center', padding:'10px'}}>
         <strong style={{fontSize:'1.5em'}}>Chat</strong>
+          You are chatting as {username} &emsp;
       </div>
       <div style={{position:'absolute', top:'4em', bottom:'4em', padding:'10px', overflow:'scroll', width:'100%', borderTop:'1px solid var(--gray)', borderBottom:'1px solid var(--gray)'}}>
         {
           chats.map(item => (
             <p>
-              <span style={{color:'var(--game-blue)'}}>{item.name}: </span>
+              <span style={{color: item.name === username ? 'var(--game-red)' : 'var(--game-blue)'}}>{item.name}: </span>
               <span> {item.text} </span>
               <span style={{color:'grey', fontSize:'0.7em'}}> {item.date}</span>
             </p>
@@ -53,7 +61,14 @@ export const ChatRoom = () => {
         }
       </div>
       <div style={{position:'absolute', bottom:'0', display:'flex', alignItems:'center', padding:'10px', width:'100%', height:'4em'}}>
-        <Input placeholder="Your message:" className="game-moonshot__input" value={message} onChange={e => { setMessage(e.currentTarget.value) }} />
+        <Input
+            autoFocus
+            placeholder="Your message:"
+            className="game-moonshot__input"
+            value={message}
+            onChange={e => { setMessage(e.currentTarget.value) }}
+            onKeyDown={(e) => e.key === "Enter" ? handleSend() : null}
+        />
         <SendOutlined style={{margin:'10px', cursor:'pointer', color:'var(--game-blue)', fontSize:'1.5em'}} onClick={handleSend} />
       </div>
 
