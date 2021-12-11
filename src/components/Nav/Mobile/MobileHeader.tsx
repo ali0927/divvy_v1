1bc4a20098f16f131c234a7d51a75588bbc6de95
ali0927 <kevinali0927@gmail.com>
2021-12-11 20:19:33 +0300

    Change activiRemove house pool Appbar Iconty table header align

--- src/components/Nav/Mobile/MobileHeader.tsx
+++ src/components/Nav/Mobile/MobileHeader.tsx
@@ -11,11 +11,16 @@ export const MobileHeader = (props: { isMobileMenuVisible: boolean, setMobileMen
             <Col className="mobile-header" span={4} style={props.isMobileMenuVisible ? {visibility: 'hidden'} : {}}>
                 <HeaderLogo />
             </Col>
-            <Col className="mobile-header" span={props.headerType !== HeaderTypes.Dashboard ? 10 : 15} style={props.isMobileMenuVisible ? {visibility: 'hidden'} : {}}>
+            <Col className="mobile-header" span={
+                (props.headerType === HeaderTypes.Dashboard) || props.headerType == HeaderTypes.Liquidity && !props.isBetSlipsVisible 
+                ? 15 : 10} style={props.isMobileMenuVisible ? {visibility: 'hidden'} : {}}>
                 <WalletBalance />
             </Col>
             { props.headerType !== HeaderTypes.Dashboard ?
-                <Col className="mobile-header" span={5} style={props.isMobileMenuVisible ? {visibility: 'hidden'} : {}}>
+                <Col className="mobile-header" span={5} style={
+                    props.isMobileMenuVisible ? {visibility: 'hidden'} :
+                    !props.isBetSlipsVisible && props.headerType == HeaderTypes.Liquidity ? {display: 'none'} : {}}
+                >
                     {props.headerType === HeaderTypes.Bets ?
                         <MobileBetSlip isBetSlipsVisible={props.isBetSlipsVisible} setBetSlipsVisible={props.setBetSlipsVisible} setMobileMenuVisible={props.setMobileMenuVisible} /> :
                         <HousePoolHeader isBetSlipsVisible={props.isBetSlipsVisible} setBetSlipsVisible={props.setBetSlipsVisible} setMobileMenuVisible={props.setMobileMenuVisible} />
